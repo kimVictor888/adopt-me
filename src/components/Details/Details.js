1b7dfeb14130f30e84c84c38995e980cd2d9a21f
Victor <kim.victor.aleksandrovich@gmail.com>
2021-12-28 01:29:35 +0600

    feat: add Details component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,7 +1,40 @@
-import React from "react";
+import { Component } from "react";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
 
-const Details = () => {
-  return <div>test</div>;
-};
+class Details extends Component {
+  constructor() {
+    super();
 
-export default Details;
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const { data } = await axios.get(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    this.setState({
+      loading: false,
+      ...data.pets[0],
+    });
+  }
+
+  render() {
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
