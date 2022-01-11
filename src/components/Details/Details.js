1682ad62a8eaf2bac23da01a2c2f67ecd093bd52
Kim Victor <kim.victor.aleksandrovich@gmail.com>
2022-01-11 13:53:04 +0600

    feat: add Modal component via Portal & useRef

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -4,9 +4,10 @@ import axios from "axios";
 import Carousel from "../Carousel/Carousel";
 import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 import ThemeContext from "../../Context/ThemeContext";
+import Modal from "../Modal/Modal";
 
 class Details extends Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   async componentDidMount() {
     const { data } = await axios.get(
@@ -18,12 +19,18 @@ class Details extends Component {
     });
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+  adopt = () => {
+    window.location = "http://bit.ly/pet-adopt";
+  };
+
   render() {
     if (this.state.loading) {
       return <h2>Loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name, images } =
+    const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
 
     return (
@@ -36,10 +43,26 @@ class Details extends Component {
           </h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                style={{ backgroundColor: theme }}
+                onClick={this.toggleModal}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
